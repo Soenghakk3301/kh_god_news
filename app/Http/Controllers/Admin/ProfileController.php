0371becd2d86a8402503a40d2b6d665b274cea06
soenghakk3301 <soenghakkchoeurn3301@gmail.com>
2023-08-01 14:58:30 +0700

    complete section of admin profile update

---
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminUpdatePasswordRequest;
+use App\Models\Admin;
+use App\Traits\FileUploadTrait;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class ProfileController extends Controller
+{
+    use FileUploadTrait;
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $user  = Auth::guard('admin')->user();
+        return view('admin.profile.index', compact('user'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        /** handle image */
+        $imagePath = $this->handleFileUpload($request, 'image', $request->old_image);
+
+        /** save updated data */
+        $admin = Admin::findOrFail($id);
+        $admin->image = !empty($imagePath) ? $imagePath : $request->old_image;
+        $admin->name = $request->name;
+        $admin->email = $request->email;
+        $admin->save();
+
+        toast(__('Update Successfully!'), 'success')->width(400);
+
+        return redirect()->back();
+    }
+
+    /** update the speicified resource for password */
+    public function passwordUpdate(AdminUpdatePasswordRequest $request, $id)
+    {
+        $admin = Admin::findOrFail($id);
+        $admin->password = bcrypt($request->password);
+        $admin->save();
+
+        toast(__('Update Successfully!'), 'success')->width(400);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Admin\AdminAuthenicationController;
 use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
@@ -21,4 +22,8 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
 Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']], function () {
     Route::get('dashboard', [DashboardController::class, 'index'])->name('dashboard');
+
+    /** profile routes */
+    Route::put('profile-password-update/{id}', [ProfileController::class, 'passwordUpdate'])->name('profile-password.update');
+    Route::resource('profile', ProfileController::class);
 });
