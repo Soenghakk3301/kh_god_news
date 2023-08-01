af38a96f69fd2e47d7a3a4b67071e88d0c9692ed
soenghakk3301 <soenghakkchoeurn3301@gmail.com>
2023-08-01 15:31:17 +0700

    complete seting up frontend

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,27 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+    <!-- Tranding news  carousel-->
+    @include('frontend.home-components.trending-news')
+    <!-- End Tranding news carousel -->
+
+    <!-- Hero Slider news -->
+    @include('frontend.home-components.hero-slider')
+    <!-- End Hero Slider news -->
+
+    <div class="large_add_banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="large_add_banner_img">
+                        <img src="images/placeholder_large.jpg" alt="adds">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Popular news category -->
+    @include('frontend.home-components.main-new')
+    <!-- End Popular news category -->
+@endsection
